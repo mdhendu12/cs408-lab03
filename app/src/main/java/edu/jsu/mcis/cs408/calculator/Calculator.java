dafe6b47aa1b17c346b0e29803f1cb3ec905ffd7
matthew <mdhendu12@gmail.com>
2023-02-14 14:03:22 -0600

    Added Model class, CalculateState enum, and an on click handler for the controller.

---
+++ app/src/main/java/edu/jsu/mcis/cs408/calculator/Calculator.java
@@ -0,0 +1,5 @@
+package edu.jsu.mcis.cs408.calculator;
+
+public class Calculator {
+    private static int leftOperand, rightOperand;
+}
--- app/src/main/java/edu/jsu/mcis/cs408/calculator/MainActivity.java
+++ app/src/main/java/edu/jsu/mcis/cs408/calculator/MainActivity.java
@@ -1,14 +1,45 @@
 package edu.jsu.mcis.cs408.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import edu.jsu.mcis.cs408.calculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        CalculatorClickHandler click = new CalculatorClickHandler();
+        ConstraintLayout layout = binding.layout;
+
+        for (int i = 0; i < layout.getChildCount(); ++i) {
+            View child = layout.getChildAt(i);
+            if(child instanceof Button) {
+                child.setOnClickListener(click);
+            }
+        }
+    }
+
+    class CalculatorClickHandler implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            String tag = ((Button) v).getTag().toString();
+            Toast toast = Toast.makeText(binding.getRoot().getContext(), tag, Toast.LENGTH_SHORT);
+            toast.show();
+            // INSERT EVENT HANDLING CODE HERE
+        }
+
     }
 }
